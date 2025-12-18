9c4f91edcb3a6a7fe0bc4a6260c7fadf60937c41
Gregory Cardinale <fgcmedia.sarl@gmail.com>
2025-12-18 17:55:20 +0100

    Comment out AccountName and HostName usage
    Comment out environment variable usage and keep original data.

--- compile.js
+++ compile.js
@@ -1,10 +1,10 @@
 var fs = require('fs');
 
-AccountName = process.env.ACCOUNT_NAME || "Developer";
-HostName = process.env.RENDER_EXTERNAL_HOSTNAME || "https://render.com";
+// AccountName = process.env.ACCOUNT_NAME || "Developer";
+// HostName = process.env.RENDER_EXTERNAL_HOSTNAME || "https://render.com";
 
 var data = fs.readFileSync('index.html', 'utf-8');
 
-var newValue = data.replace(/{\$AccountName}/gim, AccountName).replace(/{\$HostName}/gim, HostName);
+// var newValue = data.replace(/{\$AccountName}/gim, AccountName).replace(/{\$HostName}/gim, HostName);
 
-fs.writeFileSync('index.html', newValue, 'utf-8');
+fs.writeFileSync('index.html', data, 'utf-8');
